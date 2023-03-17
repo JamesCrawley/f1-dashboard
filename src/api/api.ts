b7f52bf686295bbeda17582ecdbac0d187470b30
James Crawley <jamescrawley31@gmail.com>
2023-03-17 19:06:13 +0000

    Updated serverless function to ping

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,16 +1,11 @@
-const getUrl = (path: string) =>
-  window.location.hostname === "crawley.dev"
-    ? path
-    : `http://localhost:8080/api${path}`;
-
 export const get = (path: string) => {
-  return fetch(getUrl(path), {
+  return fetch(path, {
     method: "GET",
   });
 };
 
 export const post = (path: string, body: object) => {
-  return fetch(getUrl(path), {
+  return fetch(path, {
     method: "POST",
     headers: {
       "content-type": "application/json",
