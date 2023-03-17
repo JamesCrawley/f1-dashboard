64045d9202890bf43dfee50b6342a1400b063285
James Crawley <jamescrawley31@gmail.com>
2023-03-17 17:11:02 +0000

    Update backend

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const apiRoutes = require("./router");
+
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
+
+app.use("/api", apiRoutes);
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
