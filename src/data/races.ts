b84e5b63116a6cccc6f3ba22c7233f6262c30d22
JamesCrawley <56868715+JamesCrawley@users.noreply.github.com>
2023-10-22 23:33:05 +0100

    Set COTA to completed

--- src/data/races.ts
+++ src/data/races.ts
@@ -234,7 +234,7 @@ export const races: Race[] = [
     name: "FORMULA 1 LENOVO UNITED STATES GRAND PRIX 2023",
     trackName: "Circuit of The Americas",
     country: "United States",
-    status: "in-progress",
+    status: "completed",
     result: {
       pole: "Charles Leclerc",
       first: "Max Verstappen",
